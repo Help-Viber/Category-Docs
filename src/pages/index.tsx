83542b01676276c5b1e64c5604ff4b6ff8eff8ac
Harshith-NHT <harshith.g@nighthack.in>
2026-02-13 17:12:18 +0530

    redirect url fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Redirect } from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  return <Redirect to="/docs/database-configuration" />;
+  const url = useBaseUrl('/docs/database-configuration');
+  return <Redirect to={url} />;
 }
